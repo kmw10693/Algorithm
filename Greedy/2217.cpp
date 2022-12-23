d53ae16d7a28b669a25ab153cf66fa55e8340e51
Minu Kim <kmw106933@naver.com>
2022-12-24 00:28:59 +0900

    Refactor: BOJ 2217

--- Greedy/2217.cpp
+++ Greedy/2217.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n;
-int w[100005];
+int arr[100005];
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     cin >> n;
-    for(int i=0; i<n; i++) cin >> w[i];
-    sort(w, w+n);
-    int ans=0;
-    for(int i=1; i<=n; i++)
-        ans = max(ans, w[n-i]*i);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    // 정렬
+    sort(arr, arr+n);
+    int ans = 0;
+    // 첫째 줄 부터 비교
+    for(int i=0; i<n; i++){
+        ans = max(arr[i] * (n-i), ans);
+    }
     cout << ans;
 }
