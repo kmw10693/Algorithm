7d55932ca5a3e4e67d27c4cbd023d1b266b5709a
Minu Kim <kmw106933@naver.com>
2022-12-24 06:57:54 +0900

    Feat: BOJ 1026

--- Greedy/1026.cpp
+++ Greedy/1026.cpp
@@ -1,20 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[105], b[105];
 int n;
+int a[105];
+int b[105];
 
-int main(void){
+bool compare(int i, int j) {
+    return i > j;
+}
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     cin >> n;
-    for(int i=0; i<n; i++) cin >> a[i];
-    for(int i=0; i<n; i++) cin >> b[i];
-    sort(a, a+n);
-    sort(b, b+n);
-    int ans=0;
     for(int i=0; i<n; i++){
-        ans += a[i] * b[n-1-i];
+        cin >> a[i];
     }
-    cout << ans;
+    for(int i=0; i<n; i++){
+        cin >> b[i];
+    }
+    
+    // 역정렬
+    sort(a, a+n, compare);
+    // 순정렬
+    sort(b, b+n);
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum += (a[i] * b[i]);   
+    }
+    cout << sum;
 }
